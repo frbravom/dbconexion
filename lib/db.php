aee93cbb3b0e5b9d25ce8fd8d710a7b68b113beb
cetecom <cetecom@LCV11000>
2019-04-23 15:56:43 -0300

    mejoras graficas

--- lib/db.php
+++ lib/db.php
@@ -10,6 +10,7 @@ class DBConnect{
         try{
         $dblink = new PDO("mysql:host=".DBHOST.";dbname=".DBNAME,DBUSR);
         $dblink->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $dblink->exec("set names utf8");
         return $dblink;
     }
      catch (Exception $e){
